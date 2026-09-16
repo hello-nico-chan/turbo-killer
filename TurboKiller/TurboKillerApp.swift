da76163f80af8078e52d99cf64fb785bf126b485
hello-nico-chan <hello-nico-chan@outlook.com>
2026-09-16 20:30:51 +0800

    Initial commit

---
+++ TurboKiller/TurboKillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurboKillerApp.swift
+//  TurboKiller
+//
+//  Created by NicoTech Studio on 2026/9/16.
+//
+
+import SwiftUI
+
+@main
+struct TurboKillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
